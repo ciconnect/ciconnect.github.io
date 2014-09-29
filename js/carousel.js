706563c136cc67da8563da0098c3a9f5d167bef7
ciconnect <cicjono@gmail.com>
2014-09-29 17:18:03 +0100

    Change carousel to change from 1-2 at 768

--- js/carousel.js
+++ js/carousel.js
@@ -6,20 +6,12 @@ $(document).ready(function() {
       navigation: true,
       slideSpeed: 300,
       autoPlay: 5000,
-      paginationSpeed: 2000,
+      pagination: false,
+      paginationSpeed: 1500,
       singleItem: false,
-      items: 2,
-      itemsDesktop: true,
-      itemsDesktopSmall: false,
-      itemsTablet: false,
-      itemsMobile: false
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
+      itemsCustom: [[0, 1], [768, 2]],
+      itemsScaleUp: true,
+      autoHeight: true
  
   });
 });
